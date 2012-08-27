98e717fb65bf39bdf6e5e28b49aba55c0de0c15d
William Schwartz <wkschwartz@gmail.com>
2012-08-27 00:21:17 -0400

    Convert tabs to 4 spaces
    This commit contains no changes other than s/\t/    /g

--- Percolation/Percolation.java
+++ Percolation/Percolation.java
@@ -10,137 +10,137 @@
 
 public class Percolation {
 
-	private final int N; // Length of one side of the grid.
-	private boolean[] open;
-	private WeightedQuickUnionUF paths;
-	private final int virtualTop;
-	private final int virtualBottom;
+    private final int N; // Length of one side of the grid.
+    private boolean[] open;
+    private WeightedQuickUnionUF paths;
+    private final int virtualTop;
+    private final int virtualBottom;
 
-	// create N-by-N grid, with all sites blocked
-	public Percolation(int N) {
-		// The union-find data type we're using indexes its arrays with an int,
-		// so our N^2 sized grid must have N^2 <= 2^32 - 1 <=> N > 2^16.
-		// N^2 <= 2^32 - 3 <-> N^2 < 2^32 - 2 -> N < 0xffff
-		if (N >= 0xffff)
-			throw new IllegalArgumentException("Dimension must be < 2^16");
-		this.N = N;
-		open = new boolean[N*N];
-		// Add two for the virtual top and bottom
-		paths = new WeightedQuickUnionUF(N*N + 2);
-		virtualTop = indexOf(N, N) + 1;
-		virtualBottom = indexOf(N, N) + 2;
-	}
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+        // The union-find data type we're using indexes its arrays with an int,
+        // so our N^2 sized grid must have N^2 <= 2^32 - 1 <=> N > 2^16.
+        // N^2 <= 2^32 - 3 <-> N^2 < 2^32 - 2 -> N < 0xffff
+        if (N >= 0xffff)
+            throw new IllegalArgumentException("Dimension must be < 2^16");
+        this.N = N;
+        open = new boolean[N*N];
+        // Add two for the virtual top and bottom
+        paths = new WeightedQuickUnionUF(N*N + 2);
+        virtualTop = indexOf(N, N) + 1;
+        virtualBottom = indexOf(N, N) + 2;
+    }
 
-	// is site (row i, column j) open?
-	public boolean isOpen(int i, int j) {
-		return open[indexOf(i, j)];
-	}
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        return open[indexOf(i, j)];
+    }
 
-	// is site (row i, column j) full?
-	public boolean isFull(int i, int j) {
-		return paths.connected(virtualTop, indexOf(i, j));
-	}
+    // is site (row i, column j) full?
+    public boolean isFull(int i, int j) {
+        return paths.connected(virtualTop, indexOf(i, j));
+    }
 
-	// does the system percolate?
-	public boolean percolates() {
-		return paths.connected(virtualTop, virtualBottom);
-	}
+    // does the system percolate?
+    public boolean percolates() {
+        return paths.connected(virtualTop, virtualBottom);
+    }
 
-	// open site (row i, column j) if it is not already
-	public void open(int i, int j) {
-		if (isOpen(i, j))
-			return;
-		int index = indexOf(i, j);
+    // open site (row i, column j) if it is not already
+    public void open(int i, int j) {
+        if (isOpen(i, j))
+            return;
+        int index = indexOf(i, j);
 
-		open[index] = true;
+        open[index] = true;
 
-		if (i == 1)
-			paths.union(virtualTop, index);
-		if (i == N && (N == 1 || isFull(i - 1, j)))
-			paths.union(virtualBottom, index);
+        if (i == 1)
+            paths.union(virtualTop, index);
+        if (i == N && (N == 1 || isFull(i - 1, j)))
+            paths.union(virtualBottom, index);
 
-		if (i < N  && isOpen(i + 1, j))
-			paths.union(indexOf(i + 1, j), index);
-		if (i > 1 && isOpen(i - 1, j))
-			paths.union(indexOf(i - 1, j), index);
+        if (i < N  && isOpen(i + 1, j))
+            paths.union(indexOf(i + 1, j), index);
+        if (i > 1 && isOpen(i - 1, j))
+            paths.union(indexOf(i - 1, j), index);
 
-		if (j < N && isOpen(i, j + 1))
-			paths.union(indexOf(i, j + 1), index);
-		if (j > 1 && isOpen(i, j - 1))
-			paths.union(indexOf(i, j - 1), index);
-	}
+        if (j < N && isOpen(i, j + 1))
+            paths.union(indexOf(i, j + 1), index);
+        if (j > 1 && isOpen(i, j - 1))
+            paths.union(indexOf(i, j - 1), index);
+    }
 
-	/* Convert grid coordinates of the form (x, y) where x,y in {1,...,N}
-	to an array index. E.g., indexOf(1,1) == 0; indexOf(N, N) = N^2 - 1.
+    /* Convert grid coordinates of the form (x, y) where x,y in {1,...,N}
+    to an array index. E.g., indexOf(1,1) == 0; indexOf(N, N) = N^2 - 1.
 
-	Assume the grid is in row-major form.
-	*/
-	private int indexOf(int row, int col) {
-		if (row <= 0 || row > N || col <= 0 || col > N)
-			throw new IndexOutOfBoundsException(
-					"(" + row + ", " + col + ") out of bounds "
-					+ "for " + N + "^2 grid.");
-		return (row - 1) * N + (col - 1);
-	}
+    Assume the grid is in row-major form.
+    */
+    private int indexOf(int row, int col) {
+        if (row <= 0 || row > N || col <= 0 || col > N)
+            throw new IndexOutOfBoundsException(
+                    "(" + row + ", " + col + ") out of bounds "
+                    + "for " + N + "^2 grid.");
+        return (row - 1) * N + (col - 1);
+    }
 
-	private static boolean testPercolates(int N, int[][] openSites, boolean expectation) {
-		boolean result;
-		Percolation tested = new Percolation(N);
-		for (int[] openSite: openSites)
-			tested.open(openSite[0], openSite[1]);
-		result = tested.percolates();
-		if (!result && expectation) {
-			System.err.println("Unexpected failure");
-			return false;
-		}
-		else if (result && !expectation) {
-			System.err.println("Unexpected success");
-			return false;
-		}
-		return true;
-	}
+    private static boolean testPercolates(int N, int[][] openSites, boolean expectation) {
+        boolean result;
+        Percolation tested = new Percolation(N);
+        for (int[] openSite: openSites)
+            tested.open(openSite[0], openSite[1]);
+        result = tested.percolates();
+        if (!result && expectation) {
+            System.err.println("Unexpected failure");
+            return false;
+        }
+        else if (result && !expectation) {
+            System.err.println("Unexpected success");
+            return false;
+        }
+        return true;
+    }
 
-	private static boolean testConstructorThrows(int arg, boolean expectation) {
-		boolean exceptionCaught = false;
-		try {
-			Percolation foo = new Percolation(arg);
-		}
-		catch (IllegalArgumentException e) {
-			exceptionCaught = true;
-		}
-		if (exceptionCaught && !expectation) {
-			System.err.println("Exception found for okay argument");
-			return false;
-		}
-		else if (!exceptionCaught && expectation) {
-			System.err.println("No or wrong exception for bad argument");
-			return false;
-		}
-		return true;
-	}
+    private static boolean testConstructorThrows(int arg, boolean expectation) {
+        boolean exceptionCaught = false;
+        try {
+            Percolation foo = new Percolation(arg);
+        }
+        catch (IllegalArgumentException e) {
+            exceptionCaught = true;
+        }
+        if (exceptionCaught && !expectation) {
+            System.err.println("Exception found for okay argument");
+            return false;
+        }
+        else if (!exceptionCaught && expectation) {
+            System.err.println("No or wrong exception for bad argument");
+            return false;
+        }
+        return true;
+    }
 
-	public static void main(String[] argv) {
-		int passes = 0;
-		int total = 0;
-		int[][] works = {{1, 1},		 {1, 3},
-						 {2, 1}, {2, 2},		 {2, 4},
-								  {3, 2}, {3, 3},
-						 {4, 1},		  {4, 3}};
-		total++;
-		if (testPercolates(4, works, true))
-			passes++;
+    public static void main(String[] argv) {
+        int passes = 0;
+        int total = 0;
+        int[][] works = {{1, 1},         {1, 3},
+                         {2, 1}, {2, 2},         {2, 4},
+                                  {3, 2}, {3, 3},
+                         {4, 1},          {4, 3}};
+        total++;
+        if (testPercolates(4, works, true))
+            passes++;
 
-		int[][] bad = {{1, 1},		 {1, 3},
-						{2, 1}, {2, 2},			{2, 4},
-								{3, 2},
-						{4, 1},		  {4, 3}};
-		total++;
-		if (testPercolates(4, bad, false))
-			passes++;
+        int[][] bad = {{1, 1},         {1, 3},
+                        {2, 1}, {2, 2},            {2, 4},
+                                {3, 2},
+                        {4, 1},          {4, 3}};
+        total++;
+        if (testPercolates(4, bad, false))
+            passes++;
 
-		total++;
-		if (testConstructorThrows(0x10000, true))
-			passes++;
-		System.err.println("Tests: " + passes + "/" + total);
-	}
+        total++;
+        if (testConstructorThrows(0x10000, true))
+            passes++;
+        System.err.println("Tests: " + passes + "/" + total);
+    }
 }
--- Percolation/PercolationStats.java
+++ Percolation/PercolationStats.java
@@ -9,57 +9,57 @@
  ******************************************************************************/
 
 public class PercolationStats {
-	private static final double Z95 = 1.96;
-	private final int N;
-	private double[] results;
+    private static final double Z95 = 1.96;
+    private final int N;
+    private double[] results;
 
-	// perform T independent computational experiments on an N-by-N grid
-	public PercolationStats(int N, int T) {
-		if (N <= 0 || T <= 0)
-			throw new IllegalArgumentException("Nonnegative arguments");
-		int i, j, n;
-		Percolation p;
-		this.N = N;
-		results = new double[T];
-		for (int t = 0; t < T; t++) {
-			n = 0;
-			p = new Percolation(N);
-			while (!p.percolates()) {
-				do {
-					i = StdRandom.uniform(1, N + 1);
-					j = StdRandom.uniform(1, N + 1);
-				} while(p.isOpen(i, j));
-				p.open(i, j);
-				n++;
-			}
-			results[t] = ((double) n) / (N*N);
-		}
-	}
+    // perform T independent computational experiments on an N-by-N grid
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0)
+            throw new IllegalArgumentException("Nonnegative arguments");
+        int i, j, n;
+        Percolation p;
+        this.N = N;
+        results = new double[T];
+        for (int t = 0; t < T; t++) {
+            n = 0;
+            p = new Percolation(N);
+            while (!p.percolates()) {
+                do {
+                    i = StdRandom.uniform(1, N + 1);
+                    j = StdRandom.uniform(1, N + 1);
+                } while(p.isOpen(i, j));
+                p.open(i, j);
+                n++;
+            }
+            results[t] = ((double) n) / (N*N);
+        }
+    }
 
-	// sample mean of percolation threshold
-	public double mean() {
-		return StdStats.mean(results);
-	}
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(results);
+    }
 
-	// sample standard deviation of percolation threshold
-	public double stddev() {
-		if (results.length <= 1)
-			return Double.NaN;
-		return StdStats.stddev(results);
-	}
+    // sample standard deviation of percolation threshold
+    public double stddev() {
+        if (results.length <= 1)
+            return Double.NaN;
+        return StdStats.stddev(results);
+    }
 
-	private double ci(int side) {
-		return mean() + side * (Z95 * stddev()) / Math.sqrt(results.length);
-	}
+    private double ci(int side) {
+        return mean() + side * (Z95 * stddev()) / Math.sqrt(results.length);
+    }
 
-	public static void main(String[] args) {
-		int N, T;
-		PercolationStats p;
-		N = Integer.parseInt(args[0]);
-		T = Integer.parseInt(args[1]);
-		p = new PercolationStats(N, T);
-		System.out.println("mean                    = " + p.mean());
-		System.out.println("stddev                  = " + p.stddev());
-		System.out.println("95% confidence interval = " + p.ci(-1) + ", " + p.ci(1));
-	}
+    public static void main(String[] args) {
+        int N, T;
+        PercolationStats p;
+        N = Integer.parseInt(args[0]);
+        T = Integer.parseInt(args[1]);
+        p = new PercolationStats(N, T);
+        System.out.println("mean                    = " + p.mean());
+        System.out.println("stddev                  = " + p.stddev());
+        System.out.println("95% confidence interval = " + p.ci(-1) + ", " + p.ci(1));
+    }
 }
