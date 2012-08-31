9a58e6a4588f8409a141e959d31c8d8c258abcb7
William Schwartz <wkschwartz@gmail.com>
2012-08-31 08:53:19 -0400

    RandomizedQueue class -- first draft (compiles)

---
+++ RandQ/RandomizedQueue.java
@@ -0,0 +1,107 @@
+import java.util.NoSuchElementException;
+import java.util.Iterator;
+
+public class RandomizedQueue<Item> implements Iterable<Item> {
+
+    private Item[] q;
+    private int N = 0;
+    private final static int initCapacity = 2;
+
+	// construct an empty randomized queue
+	@SuppressWarnings("unchecked")
+	public RandomizedQueue() {
+	    assert initCapacity > 0;
+	    q = (Item[]) new Object[initCapacity]; // Will produce warning. Ignore.
+    }
+
+	// is the queue empty?
+	public boolean isEmpty() {
+	    return N == 0;
+    }
+
+	// return the number of items on the queue
+	public int size() {
+	    return N;
+    }
+
+    // Resize array when necessary
+    @SuppressWarnings("unchecked")
+    private void resize(int capacity) {
+        assert capacity >= N;
+        Item[] newq = (Item[]) new Object[capacity];
+        for (int i = 0; i < q.length; i++)
+            newq[i] = q[i];
+        q = newq;
+    }
+
+	// add the item
+	public void enqueue(Item item) {
+	    if (item == null)
+	        throw new NullPointerException("Queue does not support nulls");
+	    if (N == q.length)
+	        resize(N + N);
+	    q[N++] = item;
+    }
+
+	/* delete and return a random item
+	 *
+	 * Swap the random item with the last item in the list and return that
+	 * item because otherwise we'd get holes in the array and wouldn't know
+	 * where they are.
+	 */
+	public Item dequeue() {
+	    if (N == 0)
+	        throw new NoSuchElementException("Empty queue");
+	    /* The advantage of adding 1 below is that the following lines won't
+	    halve the size of the array unless it's at least 4 in the worst case
+	    when N == 0. This is important because 4 is the smallest integer i for
+	    which i >> 2 > 0. Finally note that as N gets large, these lines only
+	    downsize when the array is a quarter full.
+	    */
+	    else if ((N + 1) * 4 < q.length)
+	        resize(N >> 2);
+	    exchange(q, StdRandom.uniform(N), --N);
+	    return q[N];
+    }
+
+    // Swap entries i & j in the array a.
+    private void exchange(Item[] a, int i, int j) {
+        if (i == j)
+            return;
+        Item swap = a[i];
+        a[i] = a[j];
+        a[j] = swap;
+    }
+
+
+	// return (but do not delete) a random item
+	public Item sample() {
+	    if (N == 0)
+	        throw new NoSuchElementException("Empty queue");
+	    return q[StdRandom.uniform(N)];
+    }
+
+	// return an independent iterator over items in random order
+	public Iterator<Item> iterator() {
+	    return new RandomizedQueueIterator();
+	}
+
+    private class RandomizedQueueIterator implements Iterator<Item>{
+	    private int tail = N;
+
+	    public boolean hasNext() {
+	        return tail > 0;
+	    }
+
+	    public Item next() {
+	        if (tail == 0)
+	            throw new NoSuchElementException();
+	        exchange(q, StdRandom.uniform(tail), --tail);
+	        return q[tail];
+	    }
+
+	    public void remove() {
+	        throw new UnsupportedOperationException("Can't remove");
+	    }
+    }
+}
