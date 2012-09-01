7b9359537850a2d853511bca868d6f0ae74c8a16
William Schwartz <wkschwartz@gmail.com>
2012-09-01 11:47:11 -0400

    Subest client application using O(n) memory

---
+++ RandQ/Subset.java
@@ -0,0 +1,8 @@
+public class Subset {
+    public static void main(String[] args) {
+        int k = Integer.parseInt(args[0]);
+        RandomizedQueue<String> q = new RandomizedQueue<String>();
+        while (!StdIn.isEmpty()) q.enqueue(StdIn.readString());
+        for (int i = 0; i < k; i++) System.out.println(q.dequeue());
+    }
+}
