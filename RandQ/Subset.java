232d2c1e906457065a39ab1137eae5b3ae45528d
William Schwartz <wkschwartz@gmail.com>
2012-09-01 18:05:15 -0400

    Stylistic change

--- RandQ/Subset.java
+++ RandQ/Subset.java
@@ -3,6 +3,7 @@ public class Subset {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> q = new RandomizedQueue<String>();
         while (!StdIn.isEmpty()) q.enqueue(StdIn.readString());
-        for (int i = 0; i < k; i++) System.out.println(q.dequeue());
+        for (int i = 0; i < k; i++)
+            System.out.println(q.dequeue());
     }
 }
